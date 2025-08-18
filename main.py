fdee9fe0be1fe6f755d4090b3e682ee95a9e9fbc
wannengguang <2842138941@qq.com>
2025-08-19 00:21:28 +0800

    fix

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from core.execute import main
-
-
-
-if __name__ == "__main__":
-    if "current_page" not in st.session_state:
-        st.session_state.current_page = "paid_record"
-    main()
